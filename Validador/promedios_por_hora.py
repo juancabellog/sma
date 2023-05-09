1da149da4776a9cdb01a081a2c8e91f6728170b4
manolocabello <manolocabello@192.168.3.5>
2023-05-09 11:31:02 -0400

    cambios para promedios por hora

---
+++ Validador/promedios_por_hora.py
@@ -0,0 +1,4 @@
+#Data es un DataFrame con 
+def promedios(data):
+    print('entra a promnedios:')
+   
--- Validador/rest-service.py
+++ Validador/rest-service.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify, session
-from validador import valida_archivo;
+from validador import valida_archivo
+from promedios_por_hora import promedios
 
 app = Flask(__name__)
 
@@ -7,3 +8,12 @@ app = Flask(__name__)
 def validaArchivo():
     errores = valida_archivo(request.get_json(True))
     return jsonify(errores)
+
+#los parámetros de entrada solo para aire: fecha, hora (0-23)
+#dataframe, ufid, idproceso, fecha timestamp, parametro, valor
+# los datos son: ufid, idProceso, fecha timestamp, parametro, valor
+@app.route("/promediosPorHora")
+def promediosPorHora():
+    print(request.args.get('fecha'), request.args.get('hora'))
+    promedios([])
+    return 'None'
